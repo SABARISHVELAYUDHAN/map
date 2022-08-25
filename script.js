18dcfe8ac2adcbf9122a884ab52bd5b1847baff0
SABARISHVELAYUDHAN <sabarishvelayudhan121@gmail.com>
2022-08-25 14:45:56 +0530

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+function myFunction(){
+    var a = document.getElementById("Fname").value;
+    document.getElementById("demo1").innerHTML = a;
+    var b = document.getElementById("Lname").value;
+    document.getElementById("demo2").innerHTML = b;
+    var c = document.getElementById("Adrs").value
+    document.getElementById("demo3").innerHTML = c;
+    var d = document.getElementById("birth").value
+    document.getElementById("demo4").innerHTML = d;
+   
+   
+    }
