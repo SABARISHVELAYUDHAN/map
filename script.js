4178b9294ca16f2d4ad02329e4563b5cb0dac512
SABARISHVELAYUDHAN <sabarishvelayudhan121@gmail.com>
2022-08-25 16:25:51 +0530

    update

--- script.js
+++ script.js
@@ -8,5 +8,13 @@ function myFunction(){
     var d = document.getElementById("birth").value
     document.getElementById("demo4").innerHTML = d;
    
-   
+        async function getIP(){
+            let response = await fetch("http://httpbin.org/get");
+            let data = await response.json()
+            document.getElementById("demo5").innerHTML = data.origin;
+            return data;
+        }
+        
+            getIP().then(data => console.log(data));
+        
     }
